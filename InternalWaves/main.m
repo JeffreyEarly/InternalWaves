a6ffbbd3eeaa76fa21e28a87a7bc58688a0314d6
Jeffrey J. Early <jearly@nwra.com>
2013-12-09 15:05:50 -0800

    Initial Commit

---
+++ InternalWaves/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  InternalWaves
+//
+//  Created by Jeffrey J. Early on 12/9/13.
+//  Copyright (c) 2013 Jeffrey J. Early. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
