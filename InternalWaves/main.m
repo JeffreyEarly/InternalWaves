1ea0422391ca571e3ff6b81cd096f46b132fa06d
Jeffrey J. Early <jearly@nwra.com>
2013-12-09 15:43:25 -0800

    Experimenting with some of the built-in functions for computing internal modes. Have yet to decide how best to proceed.

--- InternalWaves/main.m
+++ InternalWaves/main.m
@@ -7,15 +7,28 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <GLNumericalModelingKit/GLNumericalModelingKit.h>
 
 int main(int argc, const char * argv[])
 {
 
 	@autoreleasepool {
 	    
-	    // insert code here...
-	    NSLog(@"Hello, World!");
-	    
+		GLEquation *equation = [[GLEquation alloc] init];
+		NSUInteger N=32;
+		GLDimension *zDim = [[GLDimension alloc] initDimensionWithGrid: kGLEndpointGrid nPoints: N domainMin: 0.0 length: N-1];
+		zDim.name = @"z";
+		
+		GLLinearTransform *diffZZ = [GLLinearTransform finiteDifferenceOperatorWithDerivatives: 2 leftBC: kGLDirichletBoundaryCondition rightBC:kGLDirichletBoundaryCondition bandwidth:1 fromDimension:zDim forEquation:equation];
+		diffZZ = [diffZZ densified];
+		[diffZZ dumpToConsole];
+		
+		NSArray *system = [diffZZ eigensystem];
+		GLFunction *eigenvalues = system[0];
+		GLLinearTransform *S = system[1];
+		
+		[eigenvalues dumpToConsole];
+		[S dumpToConsole];
 	}
     return 0;
 }
