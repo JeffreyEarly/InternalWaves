1c4c5f4b4b1b70812a8d519cfc663a1b25407090
Jeffrey J. Early <jearly@nwra.com>
2014-01-01 20:17:10 -0800

    Fixed GLAverageOperation.
    Added an operation to create a diagonal linear transformation from a function.
    
    Generalized the -dividedBy operation for functions to handle scalars betters.

--- InternalWaves/main.m
+++ InternalWaves/main.m
@@ -9,6 +9,43 @@
 #import <Foundation/Foundation.h>
 #import <GLNumericalModelingKit/GLNumericalModelingKit.h>
 
+@interface GLInternalModes : NSObject
+
+@end
+
+@implementation GLInternalModes
+
+- (NSArray *) internalModesFromDensityProfile: (GLFunction *) rho
+{
+    if (rho.dimensions.count != 1) {
+        [NSException raise:@"InvalidDimensions" format:@"Only one dimension allowed, at this point"];
+    }
+    
+    GLEquation *equation = rho.equation;
+    GLDimension *zDim = rho.dimensions[0];
+    
+    GLLinearTransform *diffZ = [GLLinearTransform finiteDifferenceOperatorWithDerivatives: 1 leftBC: kGLDirichletBoundaryCondition rightBC:kGLDirichletBoundaryCondition bandwidth:1 fromDimension:zDim forEquation:equation];
+    
+    GLLinearTransform *diffZZ = [GLLinearTransform finiteDifferenceOperatorWithDerivatives: 2 leftBC: kGLDirichletBoundaryCondition rightBC:kGLDirichletBoundaryCondition bandwidth:1 fromDimension:zDim forEquation:equation];
+    
+    GLScalar *rho_0 = [rho mean: 0];
+    GLFunction *N2 = [diffZ transform: [rho dividedBy: rho_0]];
+    
+    GLFunction *invN2 = [N2 scalarDivide: 1.0];
+    GLLinearTransform *invN2_trans = [GLLinearTransform linearTransformFromFunction: invN2];
+    
+    GLFunction *invN2_z = [diffZ transform: invN2];
+    GLLinearTransform *invN2_z_trans = [GLLinearTransform linearTransformFromFunction: invN2_z];
+    
+    GLLinearTransform *diffOp = [[invN2_z_trans multiply: diffZ] plus: [invN2_trans multiply: diffZZ]];
+    NSArray *system = [diffOp eigensystem];
+    
+    return system;
+}
+
+@end
+
+
 int main(int argc, const char * argv[])
 {
 
@@ -18,6 +55,7 @@ int main(int argc, const char * argv[])
 		NSUInteger N=32;
 		GLDimension *zDim = [[GLDimension alloc] initDimensionWithGrid: kGLEndpointGrid nPoints: N domainMin: 0.0 length: N-1];
 		zDim.name = @"z";
+        GLFunction *z = [GLFunction functionOfRealTypeFromDimension:zDim withDimensions:@[zDim] forEquation:equation];
 		
 		GLLinearTransform *diffZZ = [GLLinearTransform finiteDifferenceOperatorWithDerivatives: 2 leftBC: kGLDirichletBoundaryCondition rightBC:kGLDirichletBoundaryCondition bandwidth:1 fromDimension:zDim forEquation:equation];
 		diffZZ = [diffZZ densified];
@@ -28,6 +66,15 @@ int main(int argc, const char * argv[])
 		GLLinearTransform *S = system[1];
 		
 		[eigenvalues dumpToConsole];
+		[S dumpToConsole];
+        
+        GLFunction *rho = [z times: @(1)];
+        GLInternalModes *internalModes = [[GLInternalModes alloc] init];
+        system = [internalModes internalModesFromDensityProfile: rho];
+        eigenvalues = system[0];
+		S = system[1];
+        
+        [eigenvalues dumpToConsole];
 		[S dumpToConsole];
 	}
     return 0;
