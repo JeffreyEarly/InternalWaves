8d7aeaca8d36f46469c1ceee4b4ce117aa37470a
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2014-06-03 09:51:36 -0700

    RandomWalk test now computes kappa from both the 2nd moment of position and from the velocity variance.

--- RandomWalk/main.m
+++ RandomWalk/main.m
@@ -13,26 +13,15 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        NSUInteger Nx = 10;
-        NSUInteger Ny = 10;
-        GLFloat Dx = 10;
-        GLFloat Dy = 10;
-        
-        /************************************************************************************************/
-        /*		Define the problem dimensions															*/
-        /************************************************************************************************/
+        NSUInteger N = 100;
+
         GLEquation *equation = [[GLEquation alloc] init];
-        GLDimension *xFloatDim = [[GLDimension alloc] initDimensionWithGrid: kGLEndpointGrid nPoints: Nx domainMin: -(Dx-1)/2 length: Dx-1];
-        xFloatDim.name = @"x";
-        GLDimension *yFloatDim = [[GLDimension alloc] initDimensionWithGrid: kGLEndpointGrid nPoints: Ny domainMin: -(Dy-1)/2 length: Dy-1];
-        yFloatDim.name = @"y";
+        GLDimension *floatDim = [[GLDimension alloc] initDimensionWithGrid: kGLEndpointGrid nPoints: N domainMin: 1 length: N];
+        floatDim.name = @"x";
         
-        NSArray *floatDimensions = @[xFloatDim, yFloatDim];
-		GLFunction *xFloat = [GLFunction functionOfRealTypeFromDimension: xFloatDim withDimensions: floatDimensions forEquation: equation];
-		GLFunction *yFloat = [GLFunction functionOfRealTypeFromDimension: yFloatDim withDimensions: floatDimensions forEquation: equation];
-        
-		GLFunction *xPosition = [GLFunction functionFromFunction: xFloat];
-		GLFunction *yPosition = [GLFunction functionFromFunction: yFloat];
+        NSArray *floatDimensions = @[floatDim];
+		GLFunction *xPosition = [GLFunction functionOfRealTypeWithDimensions: floatDimensions forEquation: equation];
+		GLFunction *yPosition = [GLFunction functionOfRealTypeWithDimensions: floatDimensions forEquation: equation];
         
         GLFloat timeStep = 10;
 		GLFloat maxTime = 1000;
@@ -59,7 +48,7 @@ int main(int argc, const char * argv[])
         GLFunction *x = newPositions[0];
         GLFunction *y = newPositions[1];
         
-        GLScalar *meanSquareSeparation = [[[[x times: x] plus: [y times: y]] mean: 2] mean: 1];
+        GLScalar *meanSquareSeparation = [[[x times: x] plus: [y times: y]] mean: 1];
         
         GLFloat kappaDeduced = (0.25)*(meanSquareSeparation.pointerValue[tDim.nPoints-1]-meanSquareSeparation.pointerValue[0])/maxTime;
         NSLog(@"kappa: %f, actual kappa: %f", kappa, kappaDeduced);
@@ -71,16 +60,7 @@ int main(int argc, const char * argv[])
 		GLScalar *vv = [[v times: v] mean];
 		GLScalar *uv = [[u times: v] mean];
 		
-		NSLog(@"(uu, vv, uv)=(%g, %g, %g)", (uu.pointerValue[0])*(timeStep/2.), (vv.pointerValue[0])*(timeStep/2.), (uv.pointerValue[0])*(timeStep/2.));
-		
-//		GLFunction *vx = [x diff:@"t"];
-//		GLFunction *vy = [y diff:@"t"];
-//		GLScalar *Vxx = [[vx times: vx] mean];
-//		GLScalar *Vyy = [[vy times: vy] mean];
-//		GLScalar *Vxy = [[vx times: vy] mean];
-//		
-//		NSLog(@"(kappa_xx, kappa_yy, kappa_xy)=(%g, %g, %g)", Vxx.pointerValue[0]*timeStep/2., Vyy.pointerValue[0]*timeStep/2., Vxy.pointerValue[0]*timeStep/2.);
-		
+		NSLog(@"(uu, vv, uv)=(%g, %g, %g)", (uu.pointerValue[0])*(timeStep), (vv.pointerValue[0])*(timeStep), (uv.pointerValue[0])*(timeStep));
     }
     return 0;
 }
