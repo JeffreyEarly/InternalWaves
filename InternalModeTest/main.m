65aed849cea12237de3d5124162f67367df36994
JeffreyEarly <jeffrey@earlyinnovations.com>
2015-03-20 14:14:22 -0700

    Added unit tests for the full dimensional eigenvalue problem. Finite difference method is now confirmed to be working (phew!). Now just need to get spectral working!

--- InternalModeTest/main.m
+++ InternalModeTest/main.m
@@ -58,11 +58,11 @@ int main(int argc, const char * argv[])
             rho_bar = [[z times: @(-N2_0*rho0/g)] plus: @(rho0)];
         }
 	    
-		GLInternalModesSpectral *internalModes = [[GLInternalModesSpectral alloc] init];
- 		[internalModes internalGeostrophicModesFromDensityProfile: rho_bar forLatitude: latitude];
+		GLInternalModes *internalModes = [[GLInternalModes alloc] init];
+ 		//[internalModes internalGeostrophicModesFromDensityProfile: rho_bar forLatitude: latitude];
         //[internalModes internalWaveModesFromDensityProfile: rho_bar wavenumber: 1 forLatitude: latitude];
         //[internalModes internalWaveModesUsingGEPFromDensityProfile: rho_bar wavenumber: 0.008 forLatitude: latitude];
-        //[internalModes internalWaveModesFromDensityProfile: rho_bar withFullDimensions:@[xDim, yDim, zDim] forLatitude: latitude];
+        [internalModes internalWaveModesFromDensityProfile: rho_bar withFullDimensions:@[xDim, yDim, zDim] forLatitude: latitude];
         
 		[internalModes.eigendepths dumpToConsole];
 //        [internalModes.eigenfrequencies dumpToConsole];
