d0c51af5aa4c0ba057b2459fd71dc6cd3594a81b
JeffreyEarly <jeffrey@jeffreyearly.com>
2016-03-29 09:09:08 -0700

    The plane wave unit test is working perfectly.

--- Matlab/InternalWaveModelPlaneWaveUnitTest.m
+++ Matlab/InternalWaveModelPlaneWaveUnitTest.m
@@ -38,17 +38,17 @@ wavemodel = InternalWaveModel([Lx, Ly, Lz], [Nx, Ny, Nz], latitude, N0);
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-k0 = 3; % k=0..Nx/2
+k0 = 4; % k=0..Nx/2
 l0 = 0; % l=0..Ny/2
 j0 = 1; % j=1..nModes, where 1 indicates the 1st baroclinic mode
-U = 1.0; % m/s
-sign = +1;
+U = 2.0; % m/s
+sign = -1;
 
 wavemodel.InitializeWithPlaneWave(k0,l0,j0,U,sign);
 
-t = 500;
+t = 1000;
 [u,v] = wavemodel.VelocityFieldAtTime(t);
-[w,zeta] = wavemodel.VerticalFieldsAtTime(0);
+[w,zeta] = wavemodel.VerticalFieldsAtTime(t);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
@@ -59,28 +59,30 @@ t = 500;
 f0 = wavemodel.f0;
 k = wavemodel.k;
 h = wavemodel.h;
+x = wavemodel.x;
 X = wavemodel.X;
 Z = wavemodel.Z;
 
 % Note that this solution is only valid for l=0.
-omega = 2*pi/wavemodel.period;
+omega = sign*2*pi/wavemodel.period;
 m = j0*pi/Lz;
 u_unit = U*cos( k(k0+1)*X + omega*t ).*cos(m*Z);
 v_unit = -(f0/omega)*U*sin( k(k0+1)*X + omega*t ) .* cos(m*Z);
 w_unit = (U*k(k0+1)/m) * sin(k(k0+1)*X + omega*t) .* sin(m*Z);
 zeta_unit = -(U*k(k0+1)/m/omega) * cos(k(k0+1)*X + omega*t) .* sin(m*Z);
 
+% Compute the relative error
 max_speed = max(max(max( sqrt(u.*u + v.*v) )));
 u_error = max(max(max(abs(u-u_unit)/max_speed)));
 v_error = max(max(max(abs(v-v_unit)/max_speed)));
-
 max_w = max(max(max( w )));
-w_error = max(max(max(abs(w-w_unit)/max_w)))
+w_error = max(max(max(abs(w-w_unit)/max_w)));
 max_zeta = max(max(max( zeta )));
-zeta_error = max(max(max(abs(zeta-zeta_unit)/max_zeta)))
+zeta_error = max(max(max(abs(zeta-zeta_unit)/max_zeta)));
 
-fprintf('The model solution for (u,v) matches the analytical solution to 1 part in (10^%d, 10^%d)\n', round(abs(log10(u_error))), round(abs(log10(v_error))));
+fprintf('The model solution for (u,v) matches the analytical solution to 1 part in (10^%d, 10^%d) at time t=%d\n', round(abs(log10(u_error))), round(abs(log10(v_error))),t);
+fprintf('The model solution for (w,zeta) matches the analytical solution to 1 part in (10^%d, 10^%d) at time t=%d\n', round(abs(log10(w_error))), round(abs(log10(zeta_error))),t);
 
 
-figure, plot( squeeze(w(8,8,:)), z)
-hold on, plot( squeeze(w_unit(8,8,:)), z)
+% figure, plot( squeeze(w(8,8,:)), z)
+% hold on, plot( squeeze(w_unit(8,8,:)), z)
